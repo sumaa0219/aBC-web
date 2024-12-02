01e3cb292ed6fac4522d2a2a3b495c10e26dab4b
sumaa0219 <sumaa0219@gmail.com>
2024-12-02 21:24:56 +0900

    サーバーキャッシュ追加

--- pages/api/getMarkdown.ts
+++ pages/api/getMarkdown.ts
@@ -8,6 +8,9 @@ const notion = new Client({
 
 const n2m = new NotionToMarkdown({ notionClient: notion });
 
+// メモリキャッシュオブジェクト
+const cache: { [key: string]: string } = {};
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const { pageId } = req.query;
 
@@ -15,10 +18,19 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         return res.status(400).json({ error: 'Invalid pageId' });
     }
 
+    // キャッシュに存在するか確認
+    if (cache[pageId]) {
+        return res.status(200).json({ markdown: cache[pageId] });
+    }
+
     try {
         const mdblocksPromise = n2m.pageToMarkdown(pageId);
         const mdblocks = await mdblocksPromise;
         const mdStringObject = n2m.toMarkdownString(mdblocks);
+
+        // キャッシュに保存
+        cache[pageId] = mdStringObject.parent;
+
         res.status(200).json({ markdown: mdStringObject.parent });
     } catch (error) {
         res.status(500).json({ error: 'Failed to fetch markdown' });
